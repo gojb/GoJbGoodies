67019578e55a6317ae895ac8a4448937d8627781
Glenns HP <Glenns@Glenns-HP>
2014-07-14 10:33:24 +0200

    aetjm

--- JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
+++ JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
@@ -287,7 +287,7 @@ public class Mouse extends JPanel implements 	ActionListener,
 		om.setLocationRelativeTo(HuvudFönster);
 		
 		HuvudFönster.setJMenuBar(MenyRad);
-//		HuvudFönster.setSize(Double.toToolkit.getDefaultToolkit().getScreenSize().getWidth()/2,Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2);
+		HuvudFönster.setSize(Double.toToolkit.getDefaultToolkit().getScreenSize().getWidth()/2,Toolkit.getDefaultToolkit().getScreenSize().getHeight()/2);
 		HuvudFönster.setLayout(new BorderLayout());
 		HuvudFönster.setMinimumSize(new Dimension(400,400));
 		HuvudFönster.addKeyListener(this);
@@ -4342,7 +4342,7 @@ class Pass implements ActionListener{
 	        		
 				} catch (Exception e1) {
 					e1.printStackTrace();
-					timer.stop();
+					
 				} 
 	            if(string.equals("Uggen0684")){
  	             passwordField.setText("Uggen0684");
