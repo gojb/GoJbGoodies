45feab2c048315fe80b2a3d4ddfb93ff41e0b7e2
Jakob <jakob_b99@live.se>
2014-08-04 09:48:50 +0200

    GoJbApplet är nu tillagd

---
+++ JakobsBraOchHa/src/applet/GoJbApplet.java
@@ -0,0 +1,15 @@
+package applet;
+
+import javax.swing.JApplet;
+
+import GoJbsBraOchHa.Mouse;
+
+@SuppressWarnings("serial")
+public class GoJbApplet extends JApplet{
+
+	public void init() {
+		super.init();
+		Mouse.main(null);
+		 
+	}
+}
