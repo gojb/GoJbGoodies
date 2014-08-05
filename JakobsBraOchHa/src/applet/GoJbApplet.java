3aec781be0427214d558c109d83174f933b03c28
Jakob <jakob_b99@live.se>
2014-08-05 20:59:41 +0200

    Applet

--- JakobsBraOchHa/src/applet/GoJbApplet.java
+++ JakobsBraOchHa/src/applet/GoJbApplet.java
@@ -7,6 +7,9 @@ import GoJbsBraOchHa.Mouse;
 @SuppressWarnings("serial")
 public class GoJbApplet extends JApplet{
 	public static void main(String[] args) {
+		new GoJbApplet();
+	}
+	public GoJbApplet() {
 		Mouse.main(null);
 	}
 }
