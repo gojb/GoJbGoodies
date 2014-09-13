888ff5b2868be5ac1099dfebb56b4af239d1bf73
Jakob <jakob_b99@live.se>
2014-09-13 23:41:46 +0200

    Mellanrumsrens

--- JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
+++ JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
@@ -3859,26 +3859,11 @@ class TicTacToe implements MouseInputListener, KeyListener, ActionListener{
 //		repaint();
 		a = 0;
 	}
-	
-	
-	public void mouseReleased(MouseEvent e) {
-	}
-
-	
-	public void mouseEntered(MouseEvent e) {
-	}
-
-	
-	public void mouseExited(MouseEvent e) {
-	}
-
-	
-	public void mouseDragged(MouseEvent e) {
-	}
-
-	
-	public void mouseMoved(MouseEvent e) {
-	}
+	public void mouseReleased(MouseEvent e) {}	
+	public void mouseEntered(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {}
+	public void mouseMoved(MouseEvent e) {}
 	
 	public void actionPerformed(ActionEvent e) {
 
