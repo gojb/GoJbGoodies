82d3789e8a3b1999d847a09e47476ee687b6680a
Jakob <jakob_b99@live.se>
2014-09-22 21:07:09 +0200

    Någon 3d grej

--- JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
+++ JakobsBraOchHa/src/GoJbsBraOchHa/Mouse.java
@@ -17,6 +17,11 @@ import javax.swing.Timer;
 import javax.swing.event.*;
 import javax.swing.text.*;
 
+import org.lwjgl.*;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.*;
+import org.lwjgl.opengl.DisplayMode;
+
 import com.sun.mail.util.*;
 
 import GoJbFrame.GoJbFrame;
@@ -82,7 +87,8 @@ public class Mouse extends JPanel implements 	ActionListener,
 							debugItem = new JMenuItem("Debug är nu: " +prop.getProperty("debug", "false")),
 							mandatItem = new JMenuItem("Simulator till riksdagsmandat"),
 							glosItem = new JMenuItem("Träna på glosor"),
-							flappyItem = new JMenuItem("Spela FlappyGojb");
+							flappyItem = new JMenuItem("Spela FlappyGojb"),
+							glItem = new JMenuItem("3d");
 
 	private JButton 		knapp1 = new JButton("Blå"),
 							knapp2 = new JButton("Grön"),
@@ -201,6 +207,7 @@ public class Mouse extends JPanel implements 	ActionListener,
 		mandatItem.addActionListener(e -> new Mandat());
 		glosItem.addActionListener(e -> new Glosor());
 		flappyItem.addActionListener(e -> new FlappyGoJb());
+		glItem.addActionListener(e -> new FullscreenExample().start());
 		
 		autoscrollknapp.addActionListener(this);
 		loggaUtItem.addActionListener(this);
@@ -237,6 +244,7 @@ public class Mouse extends JPanel implements 	ActionListener,
 		arkivMeny.add(mandatItem);
 		arkivMeny.add(glosItem);
 		arkivMeny.add(flappyItem);
+		arkivMeny.add(glItem);
 		arkivMeny.addSeparator();
 		arkivMeny.add(loggaUtItem);
 		arkivMeny.add(avslutaItem);
@@ -1567,7 +1575,7 @@ class FlappyGoJb extends JPanel implements KeyListener,WindowListener{
 	
 	FlappyGoJb(){
 		
-		setBackground(white);
+		setBackground(new Color(255, 255, 255));
 		frame.addKeyListener(this);
 		frame.setIconImage(fönsterIcon);
 		frame.add(this);
@@ -5194,5 +5202,208 @@ class DraOchSläpp implements MouseInputListener{
 	@Override
 	public void mouseMoved(MouseEvent arg0) {
 
+	}
+}
+
+class FullscreenExample {
+
+	/** position of quad */
+	float x = 400, y = 300;
+	/** angle of quad rotation */
+	float rotation = 0;
+
+	/** time at last frame */
+	long lastFrame;
+
+	/** frames per second */
+	int fps;
+	/** last fps time */
+	long lastFPS;
+
+	/** is VSync Enabled */
+	boolean vsync;
+
+	public void start() {
+		try {
+			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setTitle("_GoJbGame");
+//			Display.setIcon(new ByteBuffer[20]);
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+
+		initGL(); // init OpenGL
+		getDelta(); // call once before loop to initialise lastFrame
+		lastFPS = getTime(); // call before loop to initialise fps timer
+
+		while (!Display.isCloseRequested()) {
+			int delta = getDelta();
+
+			update(delta);
+			renderGL();
+
+			Display.update();
+			Display.sync(60); // cap fps to 60fps
+		}
+
+		Display.destroy();
+	}
+
+	public void update(int delta) {
+		// rotate quad
+		rotation += 0.15f * delta;
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) x -= 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) x += 0.35f * delta;
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) y += 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) y -= 0.35f * delta;
+
+		while (Keyboard.next()) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_F) {
+					setDisplayMode(800, 600, !Display.isFullscreen());
+				}
+				else if (Keyboard.getEventKey() == Keyboard.KEY_V) {
+					vsync = !vsync;
+					Display.setVSyncEnabled(vsync);
+				}
+			}
+		}
+
+		// keep quad on the screen
+		if (x < 0) x = 0;
+		if (x > 800) x = 800;
+		if (y < 0) y = 0;
+		if (y > 600) y = 600;
+
+		updateFPS(); // update FPS Counter
+	}
+
+	/**
+	 * Set the display mode to be used 
+	 * 
+	 * @param width The width of the display required
+	 * @param height The height of the display required
+	 * @param fullscreen True if we want fullscreen mode
+	 */
+	public void setDisplayMode(int width, int height, boolean fullscreen) {
+
+		// return if requested DisplayMode is already set
+		if ((Display.getDisplayMode().getWidth() == width) && 
+				(Display.getDisplayMode().getHeight() == height) && 
+				(Display.isFullscreen() == fullscreen)) {
+			return;
+		}
+
+		try {
+			DisplayMode targetDisplayMode = null;
+
+			if (fullscreen) {
+				DisplayMode[] modes = Display.getAvailableDisplayModes();
+				int freq = 0;
+
+				for (int i=0;i<modes.length;i++) {
+					DisplayMode current = modes[i];
+
+					if ((current.getWidth() == width) && (current.getHeight() == height)) {
+						if ((targetDisplayMode == null) || (current.getFrequency() >= freq)) {
+							if ((targetDisplayMode == null) || (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel())) {
+								targetDisplayMode = current;
+								freq = targetDisplayMode.getFrequency();
+							}
+						}
+
+						// if we've found a match for bpp and frequence against the 
+						// original display mode then it's probably best to go for this one
+						// since it's most likely compatible with the monitor
+						if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel()) &&
+								(current.getFrequency() == Display.getDesktopDisplayMode().getFrequency())) {
+							targetDisplayMode = current;
+							break;
+						}
+					}
+				}
+			} else {
+				targetDisplayMode = new DisplayMode(width,height);
+			}
+
+			if (targetDisplayMode == null) {
+				System.out.println("Failed to find value mode: "+width+"x"+height+" fs="+fullscreen);
+				return;
+			}
+
+			Display.setDisplayMode(targetDisplayMode);
+			Display.setFullscreen(fullscreen);
+
+		} catch (LWJGLException e) {
+			System.out.println("Unable to setup mode "+width+"x"+height+" fullscreen="+fullscreen + e);
+		}
+	}
+
+	/** 
+	 * Calculate how many milliseconds have passed 
+	 * since last frame.
+	 * 
+	 * @return milliseconds passed since last frame 
+	 */
+	public int getDelta() {
+		long time = getTime();
+		int delta = (int) (time - lastFrame);
+		lastFrame = time;
+
+		return delta;
+	}
+
+	/**
+	 * Get the accurate system time
+	 * 
+	 * @return The system time in milliseconds
+	 */
+	public long getTime() {
+		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
+	}
+
+	/**
+	 * Calculate the FPS and set it in the title bar
+	 */
+	public void updateFPS() {
+		if (getTime() - lastFPS > 1000) {
+//			Display.setTitle("FPS: " + fps);
+			fps = 0;
+			lastFPS += 1000;
+		}
+		fps++;
+	}
+
+	public void initGL() {
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, 800, 0, 600, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+	}
+
+	public void renderGL() {
+		// Clear The Screen And The Depth Buffer
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+
+		// R,G,B,A Set The Color To Blue One Time Only
+		GL11.glColor3f(0.5f, 0.5f, 1.0f);
+
+		// draw quad
+		GL11.glPushMatrix();
+		GL11.glTranslatef(x, y, 0);
+		GL11.glRotatef(rotation, 0f, 0f, 1f);
+		GL11.glTranslatef(-x, -y, 0);
+
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2f(x - 50, y - 50);
+		GL11.glVertex2f(x + 50, y - 50);
+		GL11.glVertex2f(x + 50, y + 50);
+		GL11.glVertex2f(x - 50, y + 50);
+		GL11.glEnd();
+		GL11.glPopMatrix();
 	}
 }
