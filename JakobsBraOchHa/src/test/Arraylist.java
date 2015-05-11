338d9110e77b210c39d00f3b381304a1de3542d7
Jakob <jakob_b99@live.se>
2015-05-11 18:58:11 +0200

    Test

---
+++ JakobsBraOchHa/src/test/Arraylist.java
@@ -0,0 +1,28 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Arraylist {
+
+	public Arraylist() {
+		ArrayList<String> strings = new ArrayList<>();
+		strings.add("hej");
+		strings.add("på");
+		strings.add("dig");
+		strings.add("hej");
+		strings.add("då");
+		strings.add("tja");
+		System.err.println(strings.toString());
+		
+		List<String> arrayList = Arrays.asList(strings.toString().substring(1,strings.toString().length()-1).split(","));
+		System.err.println(arrayList);
+		System.err.println(arrayList.get(1));
+		
+	}
+	public static void main(String[] args) {
+		new Arraylist();
+	}
+
+}
