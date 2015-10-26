4935c8e8e32b704e01541499a025e44e2a08485a
Jakob <jakob_b99@live.se>
2015-10-26 16:36:40 +0100

    Tetris är nu i stort sett färdigt, fattas gameover

--- JakobsBraOchHa/src/gojb/Tetris.java
+++ JakobsBraOchHa/src/gojb/Tetris.java
@@ -12,12 +12,11 @@ public class Tetris {
 	private GoJbFrame frame = new GoJbFrame("Tetris");
 	private int size=20,fönsterbredd=12,fönsterhöjd=20;
 	private Timer timer = new Timer(500, e-> uppdatera());
-	boolean snabb;
+	private boolean snabb;
 	private ArrayList<Block> aktuella = new ArrayList<>(), fasta = new ArrayList<>();
 
 	private JLabel label = new JLabel(){
 		private static final long serialVersionUID = 1;
-	
 		public void paintComponent(Graphics g) {
 			Graphics2D g2 = (Graphics2D)g;
 			for (Block block : aktuella) {
@@ -33,31 +32,10 @@ public class Tetris {
 		public void keyReleased(KeyEvent e) {}
 		public void keyPressed(KeyEvent e) {
 			if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
-				boolean blockera = false;
-				for (Block block : aktuella) {
-					if (block.x+1==fönsterbredd) {
-						blockera=true;
-					}
-				}
-				if (!blockera) {
-					for (Block block : aktuella) {
-						block.flyttaHöger();
-					}
-				}
-				frame.repaint();
+				höger();
 			}
 			else if (e.getKeyCode()==KeyEvent.VK_LEFT) {
-				boolean blockera = false;
-				for (Block block : aktuella) {
-					if (block.x<=0) {
-						blockera=true;
-					}
-				}
-				if (!blockera) {
-					for (Block block : aktuella) {
-						block.flyttaVänster();
-					}
-				}
+				vänster();
 			}
 			else if (e.getKeyCode()==KeyEvent.VK_DOWN) {
 				snabb=true;
@@ -65,36 +43,87 @@ public class Tetris {
 					uppdatera();
 				}
 				snabb=false;
-				
 			}
 			else if (e.getKeyCode()==KeyEvent.VK_UP) {
-				int xMax = 0,xMin=fönsterbredd,yMax=0,yMin=fönsterhöjd;
+				ArrayList<Block> old = new ArrayList<>();
 				for (Block block : aktuella) {
-					if (block.x>xMax) {
-						xMax=block.x;
-					}
-					if (block.x<xMin) {
-						xMin=block.x;
-					}
-					if (block.y>yMax) {
-						yMax=block.y;
-					}
-					if (block.y<yMin) {
-						yMin=block.y;
+					try {
+						old.add(block.clone());
+					} catch (CloneNotSupportedException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
 					}
 				}
-				
-				for (Block block : aktuella) {
-					int x = block.x;
-					block.x=4-block.y;
-					block.y=x;
+				if (upp().equals("fel")) {
+					aktuella.clear();
+					aktuella.addAll(old);
 				}
 			}
 			frame.repaint();
 		}
 		public void keyTyped(KeyEvent e) {}
 	};
-
+	public boolean vänster() {
+		for (Block block : aktuella) {
+			if (block.x-1<0) {
+				return false;
+			}
+			for (Block block2 : fasta) {
+				if (block.x-1==block2.x&&block.y==block2.y) {
+					return false;
+				}
+			}
+		}
+		for (Block block : aktuella) {
+			block.flyttaVänster();
+		}
+		return true;
+	}
+	public boolean höger() {
+		for (Block block : aktuella) {
+			if (block.x+1==fönsterbredd) {
+				return false;
+			}
+			for (Block block2 : fasta) {
+				if (block.x+1==block2.x&&block.y==block2.y) {
+					return false;
+				}
+			}
+		}
+		for (Block block : aktuella) {
+			block.flyttaHöger();
+		}
+		return true;
+	}
+	public String upp(){
+		String ret="ok";
+		int xMin,yMin;
+		xMin=aktuella.get(0).x-2;
+		yMin=aktuella.get(0).y-2;
+		for (Block block : aktuella) {
+			int x = block.x-xMin;
+			int y = block.y-yMin;
+			block.x=4-y+xMin;
+			block.y=x+yMin;
+		}
+		for (Block block : aktuella) {
+			while (block.x<0) {
+				höger();
+				System.out.println("höger");
+			}
+			while (block.x>=fönsterbredd) {
+				vänster();
+				System.out.println("vänst");
+			}
+			for (Block block2 : fasta) {
+				if (block.x==block2.x&&block.y==block2.y) {
+					ret="fel";
+					System.err.println("err");
+				}
+			}
+		}
+		return ret;
+	}
 	public void uppdatera() {
 		boolean b = false;
 		for (Block aktuell : aktuella) {
@@ -142,7 +171,7 @@ public class Tetris {
 		if (!snabb) {
 			frame.repaint();
 		}
-		
+
 	}
 	public Tetris() {
 		frame.setResizable(false);
@@ -163,30 +192,60 @@ public class Tetris {
 	}
 	public void blockskap() {
 		Random random = new  Random();
-		int i =	random.nextInt(3);
+		int i =	random.nextInt(7);
 		if (i==0) {
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.red));
 			aktuella.add(new Block(fönsterbredd/2-2, 0, Color.red));
 			aktuella.add(new Block(fönsterbredd/2-1, 0, Color.red));
-			aktuella.add(new Block(fönsterbredd/2, 0, Color.red));
 			aktuella.add(new Block(fönsterbredd/2+1, 0, Color.red));
 		}
 		else if (i==1) {
 			aktuella.add(new Block(fönsterbredd/2, 0, Color.green));
+			aktuella.add(new Block(fönsterbredd/2, -1, Color.green));
 			aktuella.add(new Block(fönsterbredd/2, 1, Color.green));
-			aktuella.add(new Block(fönsterbredd/2, 2, Color.green));
-			aktuella.add(new Block(fönsterbredd/2+1, 2, Color.green));
+			aktuella.add(new Block(fönsterbredd/2+1, 1, Color.green));
+
 		}
 		else if (i==2) {
-			aktuella.add(new Block(fönsterbredd/2, 0, Color.magenta));
 			aktuella.add(new Block(fönsterbredd/2, 1, Color.magenta));
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.magenta));
 			aktuella.add(new Block(fönsterbredd/2, 2, Color.magenta));
 			aktuella.add(new Block(fönsterbredd/2-1, 2, Color.magenta));
 		}
-
+		else if (i==3) {
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.BLUE));
+			aktuella.add(new Block(fönsterbredd/2, 1, Color.BLUE));
+			aktuella.add(new Block(fönsterbredd/2+1, 0, Color.BLUE));
+			aktuella.add(new Block(fönsterbredd/2+1, 1, Color.BLUE));
+		}
+		else if (i==4) {
+			aktuella.add(new Block(fönsterbredd/2, 1, Color.CYAN));
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.CYAN));
+			aktuella.add(new Block(fönsterbredd/2+1, 1, Color.CYAN));
+			aktuella.add(new Block(fönsterbredd/2+1, 2, Color.cyan));
+		}
+		else if (i==5) {
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.orange));
+			aktuella.add(new Block(fönsterbredd/2, 1, Color.orange));
+			
+			aktuella.add(new Block(fönsterbredd/2+1, 0, Color.orange));
+			aktuella.add(new Block(fönsterbredd/2+1, -1, Color.orange));
+		}
+		else if (i==6) {
+			aktuella.add(new Block(fönsterbredd/2, 0, Color.YELLOW));
+			aktuella.add(new Block(fönsterbredd/2, -1, Color.yellow));
+			aktuella.add(new Block(fönsterbredd/2, 1, Color.yellow));
+			aktuella.add(new Block(fönsterbredd/2+1, 0, Color.yellow));
+		}
 	}
-	class Block{
+	class Block implements Cloneable{
 		Color c;
 		int x,y;
+		@Override
+		protected Block clone() throws CloneNotSupportedException {
+			// TODO Auto-generated method stub
+			return (Block) super.clone();
+		}
 		public Block(int x,int y, Color c) {
 			this.x=x;
 			this.y=y;
@@ -199,11 +258,11 @@ public class Tetris {
 			x+=1;
 		}
 		public void rita(Graphics2D g) {
-			g.setColor(Color.black);
-			g.drawRect(x*size, y*size, size, size);
 			g.setColor(c);
 			g.fillRect(x*size, y*size, size, size);
-			
+			g.setColor(Color.black);
+			g.drawRect(x*size, y*size, size, size);
+
 		}
 		public void flyttaNer() {
 			y+=1;
