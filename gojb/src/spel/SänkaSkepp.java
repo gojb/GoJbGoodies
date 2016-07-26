f04d51590066760306e1a1230bc916adb17e46d1
Glenn <gojbmail@gmail.com>
2016-07-26 23:47:05 +0200

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- gojb/src/spel/SänkaSkepp.java
+++ gojb/src/spel/SänkaSkepp.java
@@ -109,8 +109,19 @@ public class SänkaSkepp {
 							allaOnline+=message.split("lla online =")[1].split(";")[0].split(",")[i]+", ";
 						}
 						JOptionPane.showMessageDialog(null, "De här är online : "+allaOnline.substring(0, allaOnline.length()-2));
+						new SänkaSkepp();
 						connectFrame.setVisible(true);
-						connectFrame.setLayout(new GridLayout(allaOnline.split(", ").length, 1));
+						connectFrame.setLayout(new GridLayout(0, 1));
+						try {
+							for (int i = 0; i < connectButtons.length; i++) {
+								connectButtons[i] = new JButton("");
+								connectFrame.remove(connectButtons[i]);
+							}
+						} catch (Exception e) {
+							System.err.println("sadassd");
+							System.out.println(e);
+						}
+//						allaOnline.split(", ").length
 						connectButtons = new JButton[allaOnline.split(", ").length];
 						for(int i =0;i<allaOnline.split(", ").length;i++){
 							connectButtons[i]=new JButton();
@@ -126,6 +137,7 @@ public class SänkaSkepp {
 					}
 					else if (string.equals("ingen")) {
 						JOptionPane.showMessageDialog(null, "Det är ingen annan online");
+						new SänkaSkepp();
 					}
 					System.out.println(message + " <-- Message");
 				}
