3d4346e723c50a2deb999fd5d025a14a1a9cbcda
Jakob <jakob_b99@live.se>
2016-10-31 01:37:33 +0100

    Signed-off-by: Jakob <jakob_b99@live.se>

--- gojb/src/spel/Sudoku.java
+++ gojb/src/spel/Sudoku.java
@@ -1,12 +1,14 @@
 package spel;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class Sudoku {
 
 	static ArrayList<Integer> list = new ArrayList<>();
-
+	int[][] integerss= new int[9][9];
+	int ber;
 	static Random random = new Random();
 
 	public static void main(String[] args) {
@@ -14,51 +16,73 @@ public class Sudoku {
 		new Sudoku();
 	}
 	public Sudoku(){
+		int f=0;
+		for (int i = 0; i < 9; i=i+0) {
+			if (f++==1000000) {
+				f=0;
+				i--;
+			}
+			ber:{
+				int[] ny = {1,2,3,4,5,6,7,8,9};
+				for (int j = ny.length - 1; j > 0; j--){
+					int index = random.nextInt(j + 1);
+					int a = ny[index];
+					ny[index] = ny[j];
+					ny[j] = a;
+				}
+				for (int j = 0; j < 3; j++) {
+					for (int j2 = 0; j2 < 3; j2++) {
+						for (int k = 0; k < 3; k++) {
+							for (int k2 = i%3; k2 > 0; k2--) {
+								ber++;
+								if (ny[j2+(j*3)]==integerss[i-k2][k+(j*3)]) {
+									break ber;
+								}
+							}
+							for (int k2 = i+0; k2 > 2; k2-=3) {
+								int siffra = ny[j+(j2*3)];
+								//Siffra 1-3 inom tidigare ruta
+								if (siffra==integerss[i-k2][(3*k)+j]) {
+									break ber;
+								}
+							}
+						}
+					}
+				}
+				integerss[i]=ny;
+				i++;
+				f=0;
+			}
 
-		for(int i=0;i < 81; i++){	
-			list.add(makeBoard(i,100));
 		}
-		for(int i=0;i < 81; i++){
-			list.set(i, list.get(i)+1);
-		}
-//		System.out.println(makeBoard(50));
-//		
-		for(int i=0;i < 9; i++){
-			System.out.println(list.get(0+(i*9))+ " " + list.get(1+(i*9))+ " " + list.get(2+(i*9))+ " : " + list.get(3+(i*9))+ " " + 
-					list.get(4+(i*9))+ " " + list.get(5+(i*9))+ " : " + list.get(6+(i*9))+ " " + list.get(7+(i*9))+ " " + list.get(8+(i*9)));
-		}
+		print(true);
+		System.err.println(ber);
 	}
-	public static int makeBoard(Integer number, Integer rand){
-		int k, a, b;
-		if(rand==100){
-			k = random.nextInt(9);
+	void print(boolean summa){
+		PrintStream printStream;
+		if (summa) {
+			printStream=System.out;
 		}
 		else{
-			if(rand!=8)k=rand+1;
-			else k=0;
+			printStream=System.err;
 		}
-		a = (int)((double)(number) / (9d))+1;//Rad, 1-9
-		b = (number%9);//Kolumn, 0-8
-		
-		for(int i = 0;i < b;i++){
-			System.out.println(i + " = i ;; k = "+k + " ;;list == "+list.get((9*(a-1))+i) + " EQUALS?: "+(list.get((9*(a-1))+i)==k) + " b: "+b+" a: "+a);
-			if(list.get((9*(a-1))+i)==k){
-//				System.err.println("KOLUMN FINNS");
-//				System.out.println(number);
-				return makeBoard(number, rand);
+		int b=0,
+				c=0;
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 3; j++) {
+				for (int j2 = 0; j2 < 3; j2++) {
+					printStream.print(integerss[b+j][j2+c]+" ");
+				}
+				printStream.print("  ");
 			}
-		}
-		for(int i = 1;i < a;i++){
-			System.out.println(i + " = i ;; k = "+k + " ;;list == "+list.get((9*(a-1))+i) + " EQUALS?: "+(list.get((9*(a-1))+i)==k) + " b: "+b+" a: "+a);
-			if(list.get((9*(i-1))+b)==k){
-//				System.err.println("RAD FINNS");
-//				System.out.println(number);
-				return makeBoard(number, rand);
+			c+=3;
+			if (c==9) {
+				c=0;
+				b+=3;
+				printStream.println();
 			}
+			printStream.println();
 		}
-		System.err.println("kolumn: "+b + " rad: "+a + " ");
-		System.err.println(((9*(a-1))+b));
-		
-		return k;
+		printStream.println();
 	}
 }
