ccde869699a72e092eb11d85293b8de01cea9e76
Glenn <gojbmail@gmail.com>
2016-10-31 22:23:46 +0100

    Signed-off-by: Glenn <gojbmail@gmail.com>

--- gojb/src/spel/Studs.java
+++ gojb/src/spel/Studs.java
@@ -13,7 +13,7 @@ public class Studs implements KeyListener{
 
 	GoJbFrame frame = new GoJbFrame("Studs", true, 3);
 
-	int xBlock=150, yBoll=50;
+	int xBlock=150, yBoll=50, xBoll = 245;
 
 	boolean flyttar;
 
@@ -26,8 +26,8 @@ public class Studs implements KeyListener{
 			g.fillRoundRect(xBlock, 400, 200, 20,10,10);
 
 			g.setColor(Color.red);
-			g.fillOval(245, yBoll, 10, 10);
-
+			g.fillOval(xBoll, yBoll, 10, 10);
+			g.drawRect(500, 1, 1, 500);
 		}
 	};
 
@@ -40,11 +40,33 @@ public class Studs implements KeyListener{
 		frame.add(label);
 		frame.addKeyListener(this);
 
-
 		faller = new Timer(10, e->{
 			yBoll=yBoll+2;
 			if(yBoll+10==400){
 				faller.stop();
+				if(250-xBlock>-1&&250-xBlock<201){
+					System.out.println("TRÄFF "+(150-xBlock));
+					int xVar=150-xBlock;
+					double koficient=1;
+					if(xVar!=0){ koficient = (Math.sqrt((100*100)-(xVar*xVar)))/xVar;}
+					System.err.println(koficient + " : "+xBoll + " : " + yBoll + " : "+ xVar);
+					yBoll=(int)(xVar*koficient);
+					
+					if(koficient<0){
+						xBoll=150;
+					}
+					else if(koficient>0){
+						xBoll=350;
+					}
+				
+					
+					System.err.println(koficient + " : "+xBoll + " : " + yBoll);
+					
+				}
+				else{
+					System.err.println("MISS");
+
+				}
 			}
 			frame.repaint();
 			frame.revalidate();
@@ -59,7 +81,7 @@ public class Studs implements KeyListener{
 	public void flyttaMark(Integer i) {
 		//i är antingen -1 (högerpil), eller 1 (vänsterpil)
 		timer = new Timer(10, e->{
-			if(xBlock+(3*i)>=0&&(xBlock+(3*i)+210)<=500){
+			if(xBlock+(3*i)>=0&&(xBlock+(i)+200)<482){
 				xBlock=xBlock+(3*i);
 				frame.revalidate();
 				frame.repaint();
