2d13991585f4d7d6a8314c45873ea9f0de6bee3b
Jakob <jakob_b99@live.se>
2017-12-03 15:04:12 +0100

    Börsrobot version 0.1
    Börsroboten är nu klar för ett första test i verkligheten. (med ett
    virituellt konto) Målet är att köpa riktiga bull/bear certifikat för
    OMXS30 om det visar sig att metoden är framgångsrik. Programmet ska då
    automatiskt tjäna pengar. Hur mycket man skulle kunna tjäna på en dag
    återstår att se.
    
    
    Signed-off-by: Jakob <jakob_b99@live.se>

--- gojb/src/gojb/Börsrobot.java
+++ gojb/src/gojb/Börsrobot.java
@@ -1,5 +1,10 @@
 package gojb;
 
+import java.util.Calendar;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
 /*
  * Copyright 2017 GoJb Development
  *
@@ -29,52 +34,148 @@ package gojb;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-import javax.swing.*;
-
-
+import static gojb.Börsrobot.Signal.*;
 public class Börsrobot  {
 	private String lösen;
+	private WebDriver driver;
+	double kurs,SMA10,SMA20,RSI,saldobull,saldobear;
+	int antalaffärer;
+	String senaste;
+	Signal innehav=NEUTRAL;
 	public static void main(String[] args) {
-		new Börsrobot();
-	}
-	public Börsrobot() {
-		lösen=JOptionPane.showInputDialog("Skriv in lösenordet");
-
-
-		openAvanza();
-		//		twitterSpam();
-		//		gojbSpam();
-
-	}
-
-
-	public void openAvanza() {
-		System.setProperty("webdriver.chrome.driver", "C:/Users/Jakob/Downloads/chromedriver_win32/chromedriver.exe");
-
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://avanza.se/start/forsta-oinloggad.html");
-		WebElement element;
-		driver.findElement(By.ByLinkText.linkText("Användarnamn & lösenord")).click();
-		driver.findElements(By.name("j_username")).get(1).sendKeys("jakobbjorns");
-		element = driver.findElements(By.name("j_password")).get(1);
-		element.sendKeys(lösen);
-		element.submit();
 		try {
-			Thread.sleep(1000);
+			new Börsrobot();
 		} catch (InterruptedException e) {
-			// FIXME Auto-generated catch block
 			e.printStackTrace();
+
 		}
-		driver.get("https://avanza.se/index/om-indexet.html/19002/omx-stockholm-30");
-		driver.findElement(By.ByLinkText.linkText("TA")).click();
-		driver.findElement(By.ByLinkText.linkText("SMA1020")).click();
-		//		try {
-		//			Thread.sleep(5000);
-		//		} catch (InterruptedException e) {
-		//			// FIXME Auto-generated catch block
-		//			e.printStackTrace();
-		//		}  // Let the user actually see something!
+	}
+	public enum Signal {SÄLJ,KÖP,NEUTRAL};
+	public Börsrobot() throws InterruptedException {
+
+		lösen=JOptionPane.showInputDialog("Skriv lösenord");
+		System.setProperty("webdriver.chrome.driver", "C:/Users/Jakob/Downloads/chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+		loggaIn();
+		openOMXS30();
+		laddaData();
+		Signal senaste=NEUTRAL;
+		while (true) {
+			reload();
+			laddaData();
+			Signal nysignal = NEUTRAL;
+			if (SMA10>SMA20&&kurs>SMA10) {
+				nysignal=KÖP;
+				System.out.println("Köpsignal");
+			}
+			else if (SMA10<SMA20&&kurs<SMA10) {
+				nysignal=SÄLJ;
+				System.out.println("Säljsignal");
+			}
+			if (senaste==nysignal) {
+				if (senaste==NEUTRAL)
+					System.out.println("avvaktar");
+				if(senaste!=innehav){
+					realsiera(senaste);
+				}
+			}
+			else{
+				senaste=nysignal;
+			}
+			Calendar c = Calendar.getInstance();
+			if (c.get(Calendar.HOUR_OF_DAY)==17) {
+				if (c.get(Calendar.MINUTE)==23) {
+					sälj_produkt(innehav);
+					antalaffärer++;
+					System.out.println("dagens vinst:"+(saldobear+saldobull));
+					driver.close();
+				}
+			}
+			System.out.println("BULL:"+saldobull+"  BEAR:"+saldobear+"  Antal:"+antalaffärer);
+			Thread.sleep(50000);
+		}
 		//		driver.quit();
 	}
+	private void sälj_produkt(Signal riktning) {
+		setSaldo(riktning,getSaldo(riktning)+kurs);
+		innehav=NEUTRAL;
+	}
+	private void köp_produkt(Signal riktning) {
+		setSaldo(riktning,getSaldo(riktning)-kurs);
+		innehav=riktning;
+	}
+	private void realsiera(Signal signal){
+		antalaffärer++;
+		if (innehav!=NEUTRAL) {
+			sälj_produkt(innehav);
+		}
+		köp_produkt(signal);
+	}
+	private double getSaldo(Signal riktning){
+		if (riktning==KÖP) {
+			return saldobull;
+		}
+		else if (riktning==SÄLJ) {
+			return saldobear;
+		}
+		System.err.println("FELAKTIG RIKTNING");
+		return 0;
+	}
+	private void setSaldo(Signal riktning,double value){
+		if (riktning==KÖP) {
+			saldobull=value;
+		}
+		else if (riktning==SÄLJ) {
+			saldobear=value;
+		}
+	}
+
+	public void loggaIn() throws InterruptedException {
+		driver.get("https://avanza.se/start/forsta-oinloggad.html");
+		driver.findElement(By.ByLinkText.linkText("Användarnamn & lösenord")).click();
+		driver.findElements(By.name("j_username")).get(1).sendKeys("jakobbjorns");
+		WebElement element = driver.findElements(By.name("j_password")).get(1);
+		element.sendKeys(lösen);
+		element.submit();
+		Thread.sleep(500);
+	}
+	public void openOMXS30() {
+		driver.get("https://avanza.se/index/om-indexet.html/19002/omx-stockholm-30");
+		driver.findElement(By.className("ta")).click();
+		driver.findElement(By.className("loadTemplate")).click();
+		driver.findElement(By.className("ta")).click();
+	}
+	public void laddaData(){
+		WebElement element=driver.findElement(By.className("highcharts-series")).findElement(By.tagName("path"));
+		element.getLocation();
+		element.getSize().getWidth();
+		//		driver.getMouse();
+		new Actions(driver).moveToElement(element).moveByOffset((int)Math.round(element.getSize().getWidth()/2d)+1, 10).click().build().perform();;
+		List<WebElement> pointlist=driver.findElements(By.className("pointRow"));
+		for (int i = 0; i <= 4; i++) {
+			WebElement span=pointlist.get(i);
+			//			List<WebElement> spans =span.findElements(By.tagName("span"));
+			System.out.println(span.getText());
+		}
+		senaste=pointlist.get(0).getText();
+		kurs=hämtaKurs(pointlist, 1);
+		SMA10=hämtaKurs(pointlist, 2);
+		SMA20=hämtaKurs(pointlist, 3);
+		RSI=hämtaKurs(pointlist, 4);
+		System.out.println(kurs+" "+SMA10+" "+SMA20+" "+RSI);
+	}
+	public void reload() throws InterruptedException{
+		WebElement element = driver.findElement(By.className("default"));
+		element.click();
+		Thread.sleep(1000);
+	}
+	double hämtaKurs(List<WebElement> list,int n){
+		String string=list.get(n).findElements(By.tagName("span")).get(1).getText()
+				.replaceAll(",", ".")
+				.replaceAll(" ", "");
+		return Double.parseDouble(string);
+	}
+
 }
